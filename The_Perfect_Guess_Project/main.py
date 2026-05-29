517e5752c1af3db6d777385d3e0115d035b4afb2
purnimasharma06 <purnima.sharma3773@gmail.com>
2026-05-29 10:48:44 +0530

    first commit

--- The_Perfect_Guess_Project/main.py
+++ The_Perfect_Guess_Project/main.py
@@ -5,13 +5,14 @@ guesses = 0
 n = random.randint(0, 100)
 
 while(a != n):
-    guesses += 1
     a = int(input("Enter your guess: "))
 
     if(a > n):
         print("Lower your guess. Try again.")
+        guesses += 1
 
-    else:
+    elif(a < n):
         print("Higher your guess. Try again.")
+        guesses += 1
 
 print(f"Congratulations! You guessed the number {n} in {guesses} guesses.")
