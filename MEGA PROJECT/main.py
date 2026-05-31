5573a654f6c8cb9bb83874f3cedcae74b29d82c3
purnimasharma06 <purnima.sharma3773@gmail.com>
2026-05-31 13:28:57 +0530

    First Commit

---
+++ MEGA PROJECT/main.py
@@ -0,0 +1,120 @@
+import speech_recognition as sr
+import webbrowser
+import pyttsx3 # text to speech
+import musicLibrary # for playing music
+import requests # for fetching news
+from openai import OpenAI
+from gtts import gTTS
+import pygame
+
+recognizer = sr.Recognizer() # it helps to recognize the speech and convert it into text
+#engine = pyttsx3.init() # it helps to convert text into speech
+newsapi = "NEWS_API" # news api key
+
+def speak_old(text):
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+    engine.stop()
+
+def speak(text):
+    tts = gTTS(text)
+    tts.save('temp.mp3') 
+
+    # Initialize Pygame mixer
+    pygame.mixer.init()
+
+    # Load the MP3 file
+    pygame.mixer.music.load('temp.mp3')
+
+    # Play the MP3 file
+    pygame.mixer.music.play()
+
+    # Keep the program running until the music stops playing
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
+    
+    pygame.mixer.music.unload()
+    # os.remove("temp.mp3") 
+
+def aiProcess(command):
+    client = OpenAI(api_key="API_KEY"),
+    
+
+    completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages = [
+        {"role": "system", "content": "You are a virtual assistant named jarvis skilled in general tasks like Alexa and Google Cloud. Give short responses please"},
+        {"role": "user", "content": command}
+    ]
+    )
+
+    return completion.choices[0].message.content
+
+def processCommand(c):
+    if "open google" in c.lower():
+        speak("Opening Google")
+        webbrowser.open("https://www.google.com")
+    elif "open youtube" in c.lower():
+        speak("Opening Youtube")
+        webbrowser.open("https://www.youtube.com")
+    elif "open facebook" in c.lower():
+        speak("Opening Facebook")
+        webbrowser.open("https://www.facebook.com")
+    elif "open linkedin" in c.lower():
+        speak("Opening Linkedin")
+        webbrowser.open("https://www.linkedin.com")
+    elif c.lower().startswith("play music"):
+        speak("Playing music")
+        song = c.lower().split(" ")[1]
+        link = musicLibrary.music[song]
+        webbrowser.open(link)
+    elif "news" in c.lower():
+        r = requests.get(f"https://newsapi.org/v2/top-headlines?country=us&apiKey={newsapi}")
+        if r.status_code == 200:
+            # Parse the JSON response
+            data = r.json()
+            
+            # Extract the articles
+            articles = data.get('articles', [])
+            
+            # Print the headlines
+            for article in articles:
+                speak(article['title'])
+
+    else:
+        # Let OpenAI handle the request
+        output = aiProcess(c)
+        speak(output) 
+
+
+
+if __name__ == "__main__":
+   speak("Initializing Jarvis........")
+   while True:
+        #Listen for the wake word "Jarvis"
+        # obtain audio from the microphone
+        r = sr.Recognizer()
+
+    
+        #recognize speech using google speech recognition
+        print("Recognizing...")
+        try:
+            with sr.Microphone() as source:
+                print("Listening....")
+                audio = r.listen(source, timeout=2, phrase_time_limit=1)
+            word = r.recognize_google(audio)
+            if(word.lower() == "jarvis"):
+                # print("Speaking ya")
+                speak("Yeah i am listening")
+                # Listen for command
+                with sr.Microphone() as source:
+                    print("Jarvis Active...")
+                    audio = r.listen(source)
+                    command = r.recognize_google(audio)
+
+
+                    processCommand(command)
+        
+        except Exception as e:
+            print("Error; {0}".format(e))
