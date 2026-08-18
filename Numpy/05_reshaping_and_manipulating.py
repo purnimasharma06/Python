29db370cc3faf7a807bcf31c0003a2143bdbae03
purnimasharma06 <purnima.sharma3773@gmail.com>
2026-08-18 08:38:25 +0530

    first commit

--- Numpy/05_reshaping_and_manipulating.py
+++ Numpy/05_reshaping_and_manipulating.py
@@ -8,7 +8,6 @@ arr = np.array([1, 2, 3, 4, 5, 6])
 reshaped_arr = arr.reshape(2, 3)  # reshape to 2 rows and 3 columns, it never creates any copy, it returns a new view of the original array with the specified shape
 print(reshaped_arr)
 
-
 # flattening arrays
 # flatten() method returns a copy of the array collapsed into one dimension 
 # ravel() method returns a flattened one-dimensional array, but it returns a view of the original array whenever possible, which means that modifying the raveled array will also modify the original array.
