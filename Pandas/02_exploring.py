397c40011560de47ababeb39a0bd0ea75a43d555
purnimasharma06 <purnima.sharma3773@gmail.com>
2026-08-31 10:13:06 +0530

    first commit

---
+++ Pandas/02_exploring.py
@@ -0,0 +1,17 @@
+# Understand the data set
+# Identify the problems 
+# Find any missing or duplicate values
+# Plan next steps to clean the data set
+
+# How we see rows in data 
+# df.head(number)  # Display the first number rows of the dataframe, default is 5
+# df.tail(number)  # Display the last number rows of the dataframe, default is 5
+
+# df.info()  # Display information about the dataframe, including data types and missing values- num of rows and cols, col name, data type, non null counts, memory usage of the data frame
+
+# df.describe()  # Display summary statistics of the dataframe, including count, mean, std, min, max, and quartiles for numerical columns
+
+# df.shape  # Display the number of rows and columns in the dataframe as a tuple (rows, columns)
+# df.columns  # Display the column names of the dataframe as an Index object
+
+
